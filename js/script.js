ed9f3fea4ea39c97a3c8c708a9ecee6451eeab14
harshasp009 <vishal.khialani@redcrackle.com>
2017-03-24 16:16:07 +0530

    Fixed css issue as per comment by matt

--- js/script.js
+++ js/script.js
@@ -13,5 +13,14 @@
       $(' '+ href_div +' ').show().siblings("section").hide();
 
     });
+    var current = location.pathname;
+    $('.nav li a').each(function(){
+      var $this = $(this);
+      // if the current path is like this link, make it active
+      if($this.attr('href') == current){
+        $this.addClass('active');
+      }
+    });
+
   });
 })(jQuery);
