9f68ae378bdf989ef86b7d7e971572251d8a7fcd
harshasp009 <sp.sriharsha@redcrackle.com>
2017-05-19 18:41:24 +0530

    MKTG-2179-fixed the search issue

--- js/search.js
+++ js/search.js
@@ -1,40 +1,29 @@
 (function($){
   $(document).ready(function(){
     var div_id;
-    //console.log(div_id);
-    //var get_idname = div_id.replace('#','');
-    //$(div_id + ' > div.pagination-page-' + get_idname).hide();
     $('#search').keyup(function() {
       var search_val = $(this).val().toLowerCase();
-      var active_text;
-      var div_length;
-      var active_div;
-      console.log("tab id " +div_id);
       if(div_id == undefined) {
         div_id = '#all';
       }
 
-        $(div_id + '> div.projectblock').each(function(k) {
+        $(div_id + ' > div.projectblock').each(function(k) {
           var get_class = $(this).children('div.row').find('div.projname h3').text().toLowerCase();
-          console.log(get_class + "-" + k);
           var get_idname = div_id.replace('#','');
           if(search_val != '' ){
             if(get_class.indexOf(search_val) != -1) {
-              console.log('search found' + k);
-              $('div.projectblock:nth-child(' + (k+1) +')').show();
+              $('div' + div_id + ' div.projectblock:eq('+ k +')').show();
 
               $(div_id + ' > div.pagination-page-' + get_idname).show();
 
             } else {
-              // $(get_class).fadeOut();
-              $('div.projectblock:nth-child(' + (k+1) +')').hide();
+              $('div' + div_id + ' div.projectblock:eq('+ k +')').hide();
               $(div_id + ' > div.pagination-page-' + get_idname).hide();
 
             }
           } else {
-            $('div.projectblock:nth-child(' + (k+1) +')').show();
+            $('div.projectblock:eq('+ k +')').show();
             $(div_id + ' > div.pagination-page-' + get_idname).show();
-            console.log('search string empty');
           }
         });
 
@@ -57,7 +46,6 @@
     $('ul.nav-pills li a').click(function() {
       div_id = $(this).attr('href');
       $('#search').keyup();
-      var get_idname = div_id.replace('#','');
     });
 
 
