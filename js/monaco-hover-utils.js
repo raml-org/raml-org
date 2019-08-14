d46238ebf0d42e532638cbb1b1a0ce8dce5c7eb0
Artem Kostiuk <postatum@gmail.com>
2019-08-14 13:15:45 +0300

    Implement loading libs from uses:

--- js/monaco-hover-utils.js
+++ js/monaco-hover-utils.js
@@ -1,7 +1,6 @@
 const hoverUtils = {
   genericDesc: 'For every API, start by defining which version of RAML you are using, and then document basic characteristics of you API - the title, baseURI, and version.',
   descRegexps: [
-    [/^uses:/, 'Create and pull in namespaced, reusable libraries containing data types, traits, resource types, schemas, examples and more.'],
     [/^annotationTypes:/, 'Annotations let you add vendor specific functionality without compromising your spec.'],
     [/^traits:/, 'Traits let you take advantage of code reuse and design patterns.'],
     [/^resourceTypes:/, 'Resource types let you take advantage of code reuse and design patterns.'],
@@ -9,17 +8,22 @@ const hoverUtils = {
     [/^\/((\w|\d)*[{/}]?)+:/, 'Easily define resources and methods, then add as much detail as you want. Apply traits and other patterns, or add parameters and other details specific to each call.'],
     [/^responses:/, 'Describe expected responses for multiple media types and specify data types or call in pre-defined schemas and examples. Schemas and examples can be defined via a data type, in-line, or externalized with !include.'],
     [/^description:/, 'Write human-readable, markdown-formatted descriptions throughout your RAML spec, or include entire markdown documentation sections at the root.'],
-    [/!include\s/, function (lineContent) {
-      const url = lineContent.split('!include').pop().trim()
+    [/!include\s/, function (lineContent) { return this.loadRefContent(lineContent, '!include') }],
+    [/^uses:/, function (lineContent) { return this.loadRefContent(lineContent, ':', 'Create and pull in namespaced, reusable libraries containing data types, traits, resource types, schemas, examples and more.') }]
+  ],
+  loadRefContent: function (lineContent, sep, defaultDesc) {
+    const url = lineContent.split(sep).slice(1).join(sep).trim()
+    if (url.length < 1) {
+      return Promise.resolve(defaultDesc)
+    }
+    return this.fetchText(url).then(text => {
       let ext = url.split('.').pop()
       if (ext === 'xsd') {
         ext = 'xml'
       }
-      return this.fetchText(url).then(text => {
-        return '```' + ext + '\n' + text + '\n```'
-      })
-    }]
-  ],
+      return '```' + ext + '\n' + text + '\n```'
+    })
+  },
   /**
    * Hover Provider for Monaco which calls other functions.
    *
@@ -36,14 +40,14 @@ const hoverUtils = {
     } else {
       desc = Promise.resolve(desc)
     }
-    return desc.then(des => {
+    return desc.then(descVal => {
       return {
         range: new monaco.Range(
           descLineNum, model.getLineMinColumn(descLineNum),
           position.lineNumber, model.getLineMaxColumn(position.lineNumber)
         ),
         contents: [
-          { value: des }
+          { value: descVal }
         ]
       }
     })
